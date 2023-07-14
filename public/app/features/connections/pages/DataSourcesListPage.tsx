583d0e4fab190a6a4ccdc6c0b3e347334110cdb4
Miklós Tolnai <miklos.tolnai@grafana.com>
2023-07-14 09:46:42 +0200

    move DataSourcesList from datasources/ to connections/

--- public/app/features/connections/pages/DataSourcesListPage.tsx
+++ public/app/features/connections/pages/DataSourcesListPage.tsx
@@ -2,9 +2,10 @@ import * as React from 'react';
 
 import { Page } from 'app/core/components/Page/Page';
 import { DataSourceAddButton } from 'app/features/datasources/components/DataSourceAddButton';
-import { DataSourcesList } from 'app/features/datasources/components/DataSourcesList';
 import { getDataSourcesCount } from 'app/features/datasources/state';
 import { StoreState, useSelector } from 'app/types';
+
+import { DataSourcesList } from '../components/DataSourcesList';
 
 export function DataSourcesListPage() {
   const dataSourcesCount = useSelector(({ dataSources }: StoreState) => getDataSourcesCount(dataSources));
